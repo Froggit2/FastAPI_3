f14cc5bc3c38438a8b06752e65f8aefc4f48985d
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-09-04 10:43:59 +0300

    Add files via upload

--- Homew_16_3.py
+++ Homew_16_3.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel
 from typing import List
 
-app = FastAPI
+app = FastAPI()
 
 users = []
 
@@ -13,33 +13,31 @@ class User(BaseModel):
     age: int
 
 
-@app.get("/user")
+@app.get("/user", response_model=List[User])
 async def users_list() -> List[User]:
     return users
 
 
-@app.post("/user/{username}/{age}")
+@app.post("/user")
 async def user_add(user: User):
     user.id = len(users)
     users.append(user)
-    return "User created"
+    return user
 
 
-@app.put("/user/{user_id}/{username}/{age}")
-async def user_update(user_id: int, username: str = Body(), age: int = Body()):
-    try:
-        update_user = users[user_id]
-        update_user.username = username
-        update_user.age = age
-    except:
+@app.put("/user/{user_id}")
+async def user_update(user_id: int, username: str = Body(...), age: int = Body(...)):
+    if user_id >= len(users) or user_id < 0:
         raise HTTPException(status_code=404, detail="User not found")
+    update_user = users[user_id]
+    update_user.username = username
+    update_user.age = age
+    return update_user
 
 
 @app.delete("/user/{user_id}")
-async def user_delete(user_id):
-    try:
-        users.pop(user_id)
-    except:
+async def user_delete(user_id: int):
+    if user_id >= len(users) or user_id < 0:
         raise HTTPException(status_code=404, detail="User not found")
-
-# python -m uvicorn Homew_16_3:app
+    deleted_user = users.pop(user_id)
+    return deleted_user
